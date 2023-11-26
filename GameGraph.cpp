13299a8f23fa8f06aa15f21cb5c04fa990c5146d
supergoodname77 <epascuales@outlook.com>
2023-11-26 11:21:46 -0600

    Update GameGraph.cpp

--- GameGraph.cpp
+++ GameGraph.cpp
@@ -24,10 +24,10 @@ GameGraph::GameGraph()
     
     for(i=0; i<20; i++)
     {
-        m_Rms[i].m_sRoomName[64] = {'n'};
-        m_Rms[i].m_sRoomDesc[128] = {'n'};
-        m_Rms[i].m_sItemName[32] = {'n'};
-        m_Rms[i].m_sCreatureName[64] = {'n'};
+        m_Rms[i].m_sRoomName[64] = '\0';
+        m_Rms[i].m_sRoomDesc[128] = '\0';
+        m_Rms[i].m_sItemName[32] = '\0';
+        m_Rms[i].m_sCreatureName[64] = '\0';
     }
     
 }
@@ -35,13 +35,114 @@ GameGraph::GameGraph()
 
 GameGraph::~GameGraph()
 {
-    
+    delete[] m_cAdjMatrix;
+    delete[] m_Rms;
 }
 
 
 bool GameGraph::doCommand(char *cmd)
 {
-    return false;
+    
+    char command[20];
+    char command2[20];
+    sscanf(cmd, "%s", command);
+    if(strcmp(command, "QUIT") == 0)
+    {
+        exit(1);
+    }
+    else if(strcmp(command, "FIGHT") == 0)
+    {
+        cout << "Fight is not implemented." << endl;
+        return false;
+    }
+    else if(strcmp(command, "TAKE") == 0)
+    {
+        cout << "Take is not implemented." << endl;
+        return false;
+    }
+    else if(strcmp(command, "GO") == 0)
+    {
+        sscanf(cmd,"%s%s", command, command2);
+        switch(command2[0])
+        {
+            case 'N':
+                if(FindDoor(m_iLocation, 'N') == true)
+                    {
+                        m_iLocation = GoDoor(m_iLocation, 'N');
+                        describeRoom(m_iLocation);
+                        break;
+                    }
+                else
+                {
+                    cout << "You can't go in that direction" << endl;
+                    break;
+                }
+            case 'S':
+                if(FindDoor(m_iLocation, 'S') == true)
+                    {
+                        m_iLocation = GoDoor(m_iLocation, 'S');
+                        describeRoom(m_iLocation);
+                        break;
+                    }
+                else
+                {
+                    cout << "You can't go in that direction" << endl;
+                    break;
+                }
+            case 'E':
+                if(FindDoor(m_iLocation, 'E') == true)
+                    {
+                        m_iLocation = GoDoor(m_iLocation, 'E');
+                        describeRoom(m_iLocation);
+                        break;
+                    }
+                else
+                {
+                    cout << "You can't go in that direction" << endl;
+                    break;
+                }
+             case 'W':
+                if(FindDoor(m_iLocation, 'W') == true)
+                    {
+                        m_iLocation = GoDoor(m_iLocation, 'W');
+                        describeRoom(m_iLocation);
+                        break;
+                    }
+                else
+                {
+                    cout << "You can't go in that direction" << endl;
+                    break;
+                }
+            case 'U':
+                if(FindDoor(m_iLocation, 'U') == true)
+                    {
+                        m_iLocation = GoDoor(m_iLocation, 'U');
+                        describeRoom(m_iLocation);
+                        break;
+                    }
+                else
+                {
+                    cout << "You can't go in that direction" << endl;
+                    break;
+                }
+            case 'D':
+                if(FindDoor(m_iLocation, 'D') == true)
+                    {
+                        m_iLocation = GoDoor(m_iLocation, 'D');
+                        describeRoom(m_iLocation);
+                        break;
+                    }
+                else
+                {
+                    cout << "You can't go in that direction" << endl;
+                    break;
+                }
+        }
+    }
+    else
+    {
+        cout << "Invalid Command" << endl;
+    }
 }
 
 void GameGraph::PrintAll()
@@ -62,75 +163,84 @@ void GameGraph::describeRoom(int roomIdx)
     cout << m_Rms[roomIdx].m_sRoomName << endl;
     cout << m_Rms[roomIdx].m_sRoomDesc << endl;
     cout << m_Rms[roomIdx].m_sItemName << endl;
-    cout << m_Rms[roomIdx].m_sCreatureName << endl;    
-    if(FindDoor('N') == true)
+    cout << m_Rms[roomIdx].m_sCreatureName << endl;
+    int i;
+    /*
+    for(i = 0; i<20; i++)
+    {
+        cout << m_cAdjMatrix[roomIdx][i] << endl;
+    }
+    */
+    
+    if(FindDoor(roomIdx, 'N') == true)
     {
         cout << "There is a door in the North wall." << endl;
     }
-    if(FindDoor('S') == true)
+    if(FindDoor(roomIdx, 'S') == true)
     {
         cout << "There is a door in the South wall." << endl;
     }
-    if(FindDoor('E') == true)
+    if(FindDoor(roomIdx, 'E') == true)
     {
         cout << "There is a door in the East wall." << endl;
     }
-    if(FindDoor('W') == true)
+    if(FindDoor(roomIdx, 'W') == true)
     {
         cout << "There is a door in the West wall." << endl;
     }
-    if(FindDoor('U') == true)
+    if(FindDoor(roomIdx, 'U') == true)
     {
         cout << "There is a stairway leading up." << endl;
     }
-    if(FindDoor('D') == true)
+    if(FindDoor(roomIdx, 'D') == true)
     {
         cout << "There is a stairway leading down." << endl;
     }
     cout << endl;
 }
-bool GameGraph::FindDoor(char cardinal)
+bool GameGraph::FindDoor(int CurrentRoom, char cardinal)
 {
     int i;
-    char caseref;
+    char caseref[1];
+    caseref[0] = {'\0'};
     for(i = 0; i<20; i++)
     {
-        caseref = m_cAdjMatrix[m_iLocation][i];
-        switch(caseref)
+        caseref[0] = m_cAdjMatrix[CurrentRoom][i];
+        switch(caseref[0])
         {
             case 'N':
-                if(caseref == cardinal)
+                if(caseref[0] == cardinal)
                 {
                     return true;
                 }
                 break;
 
             case 'S':
-                if(caseref == cardinal)
+                if(caseref[0] == cardinal)
                 {
                     return true;
                 }
                 break;
             case 'E':
-                if(caseref == cardinal)
+                if(caseref[0] == cardinal)
                 {
                     return true;
                 }
                 break;
             case 'W':
-                if(caseref == cardinal)
+                if(caseref[0] == cardinal)
                 {
                     return true;
                 }
                 break;
             case 'U':
-                if(caseref == cardinal)
+                if(caseref[0] == cardinal)
                 {
                     return true;
                 }
                 break;
             case 'D':
-                if(caseref == cardinal)
+                if(caseref[0] == cardinal)
                 {
                     return true;
                 }
@@ -138,7 +248,62 @@ bool GameGraph::FindDoor(char cardinal)
             case '\0':
                 break;
         }
+        
     }
+}
+int GameGraph::GoDoor(int CurrentRoom, char cardinal)
+{
+    int i;
+    char caseref[1];
+    caseref[0] = {'\0'};
+    for(i = 0; i<20; i++)
+    {
+        caseref[0] = m_cAdjMatrix[CurrentRoom][i];
+        switch(caseref[0])
+        {
+            case 'N':
+                if(caseref[0] == cardinal)
+                {
+                    return i;
+                }
+                break;
+
+            case 'S':
+                if(caseref[0] == cardinal)
+                {
+                    return i;
+                }
+                break;
+            case 'E':
+                if(caseref[0] == cardinal)
+                {
+                    return i;
+                }
+                break;
+            case 'W':
+                if(caseref[0] == cardinal)
+                {
+                    return i;
+                }
+                break;
+            case 'U':
+                if(caseref[0] == cardinal)
+                {
+                    return i;
+                }
+                break;
+            case 'D':
+                if(caseref[0] == cardinal)
+                {
+                    return i;
+                }
+                break;
+            case '\0':
+                break;
+        }
+        
+    }
+    return -1;
 }
 
 //---------------------------------------
@@ -151,7 +316,7 @@ bool GameGraph::LoadGame(char *filename)
     char line[128];
     int link;
     bool done;
-    //char *tempRoomline;
+    char *tempRoomline;
 
 
     m_InFile.open(filename, ifstream::in); 
@@ -166,25 +331,34 @@ bool GameGraph::LoadGame(char *filename)
     done = false;
     for(int i=0; i<20; i++)
     {
-        cout << "The File is successfully being read" << endl;
+        //cout << "The File is successfully being read" << endl;
         // Read room name
-        getNextLine(line, 128);
-        m_Rms[i].m_sRoomName[64] = *line;
+        getNextLine(line, 128); //128
+        tempRoomline = &m_Rms[i].m_sRoomName[0];
+        strcpy(tempRoomline, line);
+        //cout << m_Rms[i].m_sRoomName <<endl;
         /* --- Copy room name into data structure i --- */
 
         // Read room description
         getNextLine(line, 128);
-        m_Rms[i].m_sRoomDesc[128] = *line;
+        tempRoomline = &m_Rms[i].m_sRoomDesc[0];
+        strcpy(tempRoomline, line);
+        //cout << m_Rms[i].m_sRoomDesc <<endl;
         /* --- Copy room description into data structure i --- */
 
         // Read room item
         getNextLine(line, 128);
-        m_Rms[i].m_sItemName[32] = *line;
+        tempRoomline = &m_Rms[i].m_sItemName[0];
+        strcpy(tempRoomline, line);
+        //cout << m_Rms[i].m_sItemName <<endl;
         /* --- Copy room item name into data structure i --- */
 
         // Read room creature
         getNextLine(line, 128);
-        m_Rms[i].m_sCreatureName[64] = *line;
+        //m_Rms[i].m_sCreatureName[64] = *line;
+        tempRoomline = &m_Rms[i].m_sCreatureName[0];
+        strcpy(tempRoomline, line);
+        //cout << m_Rms[i].m_sCreatureName <<endl;
         /* --- Copy room creature name into data structure i --- */
 
         // Read North door
@@ -272,3 +446,4 @@ void GameGraph::getNextLine(char *line, int lineLen)
         }
     } // end while
 }
+
